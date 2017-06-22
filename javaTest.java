455c38cce767ae1af8cc42ea3832b039db056979
PamuLi <lijunpeng0822@outlook.com>
2017-06-22 13:58:45 +0800

    third

--- javaTest.java
+++ javaTest.java
@@ -3,5 +3,6 @@ public Class FirstJava{
 		System.out.println("This is first Java Git!");
 		System.out.println("This java file was changed.");
 		System.out.println("This java file was changed twice.");
+		System.out.println("This java file was changed third.");
 	}
 }
